8993fe7bc739a99997af28e78e642c4f3b248dee
bastienapp <29610890+bastienapp@users.noreply.github.com>
2023-07-13 10:42:14 +0200

    fixes

--- src/main/java/com/example/springsecuritydemo/dto/LoginResponse.java
+++ src/main/java/com/example/springsecuritydemo/dto/LoginResponse.java
@@ -2,6 +2,6 @@ package com.example.springsecuritydemo.dto;
 
 import com.example.springsecuritydemo.entity.User;
 
-// constructeur vide, constructeur "plein", getter et setter
+// record: constructeur vide, constructeur "plein", getter et setter
 public record LoginResponse(String token, User user) {
 }
